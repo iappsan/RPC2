41f00a783f47d51a9ba0241cc6ca44d2cf65b344
Ian SR <iann.ipn9@gmail.com>
2021-12-17 19:37:50 -0600

    todo listo y jalando

--- cliente.py
+++ cliente.py
@@ -1,11 +1,125 @@
 import xmlrpc.client
 
-def main(args):
-    proxy = xmlrpc.client.ServerProxy("http://localhost:5432")
-    n1= input()
-    n2= input()
-    print ("Hola")
-    print (proxy.test(n1,n2))
+def main():
+    proxy = xmlrpc.client.ServerProxy("http://172.16.8.23:5432")
+
+    usr = ''
+    clientState = 1
+    clientPath = ''
+    clientRootPath = ''
+    # 1 - Autenticar cliente
+    # 2 - Conexion remota
+    # 3 - Salir
+
+    while not clientState == 3:
+        while clientState == 1:
+            usr = input('Ingresa tu usuario:')
+            psswd = input('Ingresa tu contrasena:')
+            if (proxy.auth(usr, psswd)):
+                clientRootPath = usr+'/'
+                clientPath = clientRootPath
+                print (f'Usuario {usr} ingresado\n')
+                clientState = 2
+            else:
+                print ('Usuario o contrasena incorrectos\n')
+        
+        while clientState == 2:
+            remoteOption = 0
+            print (f'\nHola {usr}, que opcion quieres?\n')
+            print ('1 .Renombrar archivo o carpeta')
+            print ('2 .Borrar archivo')
+            print ('3 .Crear carpeta')
+            print ('4 .Borrar carpeta')
+            print ('5 .Listar directorio')
+            print ('6 .Cambiar de directorio')
+            print ('7 .Modificar archivo')
+            print ('8 .Crear archivo')
+            print ('9 .PWD')
+            print ('10 .Salir')
+            remoteOption = int(input())
+
+            if remoteOption == 1:
+                print ('\n---------------------\n')
+                print ('1 .Renombrar')
+                oFile = clientPath + input('Nombre actual del archivo: ')
+                nFile = clientPath + input('Nuevo nombre: ')
+                print (proxy.rename(oFile, nFile))
+
+            elif remoteOption == 2:
+                print ('\n---------------------\n')
+                print ('2 .Borrar archivo')
+                fileName = clientPath + input('Archivo a borrar: ')
+                print (proxy.remove(fileName))
+
+            elif remoteOption == 3:
+                print ('\n---------------------\n')
+                print ('3 .Crear carpeta')
+                fileName = clientPath + input('Nombre de la carpeta: ')
+                print (proxy.mkdir(fileName))
+
+            elif remoteOption == 4:
+                print ('\n---------------------\n')
+                print ('4 .Borrar carpeta')
+                fileName = clientPath + input('Nombre de la carpeta: ')
+                print (proxy.rmdir(fileName))
+
+            elif remoteOption == 5:
+                print ('\n---------------------\n')
+                print (f'5 .Listar directorio\n\t{clientPath}')
+                print (proxy.ls(clientPath))
+
+            elif remoteOption == 6:
+                print ('\n---------------------\n')
+                print ('6 .Cambiar de directorio')
+                newDir = input('A que carpeta? \n(Home - Para ir a tu raiz)\n')
+                if newDir == 'Home':
+                    clientPath = clientRootPath
+                    print(f'Nuevo direcrotio: {clientPath}')
+                else:
+                    if proxy.cd(clientPath + newDir):
+                        clientPath = clientRootPath+newDir+'/'
+                        print(f'Nuevo direcrotio: {clientPath}')
+                    else:                
+                        print (f'Carpeta {newDir} no encontrada')
+
+            elif remoteOption == 7:
+                print ('\n---------------------\n')
+                print ('7 .Modificar archivo')
+                fileName = clientPath + input('Nombre del archivo: ')
+                modStatus = 0
+                # 0 - Edita
+                # 1 - Salir
+                while modStatus == 0:
+                    print ('Archivo actual:\n')
+                    fileLines = proxy.read(fileName)
+                    for line in fileLines:
+                        print(line)
+
+                    rorw = input('Escribe lo que quieras agregar:\n(exit() para salir)\n')
+                    if rorw == 'exit()':
+                        modStatus = 1
+                    else:
+                        proxy.write(fileName, rorw)
+                        print ('\n\n')
+    
+            elif remoteOption == 8:
+                print ('\n---------------------\n')
+                print ('8 .Crear archivo')
+                fileName = clientPath + input('Nombre del archivo: ')
+                print (proxy.createFile(fileName))
+
+            elif remoteOption == 9:
+                print ('\n---------------------\n')
+                print ('9 .PWD')
+                print (f'Directorio actual: {clientPath}')
+
+            elif remoteOption == 10:
+                print ('Saliendo ...')
+                clientState = 3
+
+            else:
+                print('Opcion no valida')
+                
 
 if __name__ == '__main__':
     main()
--- serv.py
+++ serv.py
@@ -1,49 +1,134 @@
 from xmlrpc.server import SimpleXMLRPCServer
+import os
 
-def auth(user, password):
-    pass
+userList = []
 
-def createFile(newName):
-    pass
+def verifyUserFile(usr):    # Verifica la carpeta del usuario
+    try:
+        os.chdir(usr)
+        os.chdir('..')
 
-def rnw(fileName, text):
-    pass
+    except:
+        os.mkdir(usr)
 
-def rename(oldFile, newFile):
-    pass
+def auth(usr, password):    # Autenticar usuario
+    global userList
+    credOk = False
+    for user in userList:
+        if user[0] == usr:
+            if user[1] == password:
+                credOk = True
 
-def remove(fileToRm):
-    pass
+    if credOk:
+        verifyUserFile(usr)
+        return (True)
+    else:
+        return (False)
 
-def mkdir(folderName):
-    pass
+def rename(oFile, nFile):   # Renombrar 1
+    str = ''
+    try:
+        os.rename(oFile, nFile)
+        str = f'archivo {oFile} renombrado'
+    except:
+        str = 'No se pudo renombrar'
+    return str
 
-def rmdir(folderName):
-    pass
+def remove(fileToRm):   #Borrar archivo 2
+    str = ''
+    try:
+        os.remove(fileToRm)
+        str = f'archivo {fileToRm} removido'
+    except:
+        str = 'No existe el archivo {fileToRm}'
+    return str
 
-def ls():
-    pass
+def mkdir(folderName):  #Crear directorio 3
+    str = ''
+    try:
+        os.mkdir(folderName)
+        str = f'folder {folderName} creado'
+    except:
+        str = f'Ya existe {folderName}'
+    return str
 
-def cd(cdDir):
-    pass
+def rmdir(folderName):  #Borrar directorio 4
+    str = ''
+    try:
+        os.rmdir(folderName)
+        str = f'archivo {folderName} removido'
+    except:
+        str = f'No existe el folder {folderName}'
+    return str
 
-def test(a, b):
-    return a+b
+def ls(homeDir):               #Listar 5
+    print (pwd())
+    output = os.popen(f'ls {homeDir}').read()
+    return (output)
 
+def cd(cdDir):          #Cambiar directorio 6
+    try:
+        os.chdir(cdDir)
+        print (pwd())
+        str2 = cdDir.split('/')
+        for i in range(0,len(str2)):
+            os.chdir('..')
+        print (pwd())
+        print('-----------------\n')
+        return True
+    except:
+        return False
+
+def pwd():              #Imprime directorio
+    output = os.popen('pwd').read()
+    return output
+
+def read(fileName):    # Modifica archivo 7
+    str = ''
+    try:
+        str = open(fileName, 'r').readlines()
+    except:
+        str = 'Error durante lectura de archivo'
+    return str
+
+def write(fileName, text):
+    str = ''
+    try:
+        myFile = open(fileName, 'a')
+        myFile.write(text)
+        myFile.close()
+    except:
+        str = 'Error durante escritura de archivo'
+    return str
+
+def createFile(newName):    # Crea un archivo 8
+    os.system(f'touch {newName}')
+    return (f'Archivo {newName} creado')
 
 def main():
+    global userList
     print ("Iniciando servidor ...")
-    server = SimpleXMLRPCServer(("localhost", 5432))
+    server = SimpleXMLRPCServer(("172.16.8.23", 5432))
     server.register_function(auth)
     server.register_function(createFile)
-    server.register_function(rnw)
+    server.register_function(read)
+    server.register_function(write)
     server.register_function(rename)
     server.register_function(remove)
     server.register_function(mkdir)
     server.register_function(rmdir)
     server.register_function(ls)
     server.register_function(cd)
-    server.register_function(test)
+
+    uFile = open('users', 'r').readlines()
+    uList = []
+    for u in uFile:
+        uList.append(u.replace('\n',''))
+    for user in uList:
+        userList.append(user.split('|'))
+    
+    os.chdir('./Users/')
+
     print ("Servidor listo")
     server.serve_forever()
 
